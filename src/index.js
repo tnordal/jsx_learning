c06693fda5a716808c759ac21507b23c572fe06c
Tom Nordal <tnordal@gmail.com>
2018-12-08 22:55:39 +0100

    Finish section 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 // Import the React and ReactDom
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { blue } from 'ansi-colors';
+
+// function getButtonText() {
+//     return 'Click Me Again!';
+// }
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>;
+    const buttonText = {text: 'Click Me!'}
+    const style = {backgroundColor: 'blue', color: 'white'};
+    const labelText = 'Enter Name:';
+    return (
+      <div>
+        <label className="label" htmlFor="name">
+            {labelText}
+        </label>
+        <input id="name" type="text" />
+        <button style={style}>
+            {buttonText.text}
+        </button>
+      </div>  
+    );
 };
 
 // Take the react component and show it on the screen
